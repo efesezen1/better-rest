6bf3855a626c385b6a23823433865c56d91ccea9
Efe Sezen <efesezen1@gmail.com>
2024-12-24 19:22:15 +0300

    Initial Commit

---
+++ better-rest/better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_restApp.swift
+//  better-rest
+//
+//  Created by Efe Sezen on 24.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ better-restTests/better_restTests.swift
@@ -0,0 +1,17 @@
+//
+//  better_restTests.swift
+//  better-restTests
+//
+//  Created by Efe Sezen on 24.12.2024.
+//
+
+import Testing
+@testable import better_rest
+
+struct better_restTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
